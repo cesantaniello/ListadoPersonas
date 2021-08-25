ada0ac5315eef18385f0e65b9bc883acb5dd6b69
Administrator <admin@example.com>
2021-08-25 11:21:13 +0200

    4.Add function agregarPersona

--- js/app.js
+++ js/app.js
@@ -11,3 +11,18 @@ function mostrarPersonas() {
     }
     document.getElementById('personas').innerHTML = texto;
 }
+
+function agregarPersona() {
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+
+    if(nombre.value !='' && apellido.value !='') {
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona);
+        mostrarPersonas();
+    }else{
+        console.log('No hay información que agregar');
+    }
+}

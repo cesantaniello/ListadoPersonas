d28546897eca0e767497335d77c2164e444ed621
Administrator <admin@example.com>
2021-08-25 10:58:58 +0200

    3.Add classes persona and app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('app.js');
+const personas = [
+    new Persona('Carlos', 'Pérez'),
+    new Persona('Juan', 'Lara')
+];
+
+function mostrarPersonas() {
+    let texto = '';
+    for(let persona of personas) {
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
--- js/persona.js
+++ js/persona.js
@@ -1,1 +1,18 @@
-console.log('clase persona');
+class Persona{
+    constructor(nombre, apellido) {
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre() {
+        return this._nombre;
+    }
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+    get apellido() {
+        return this._apellido;
+    }
+    set apellido(apellido) {
+        this._apellido = apellido;
+    }
+}
